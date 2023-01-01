a6cb5e86a77af8bece1646b0defb5974cdd18f02
tetsu <none>
2023-01-01 15:50:55 +0900

    add file size 50kb filter

--- get_html.py
+++ get_html.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome import service as fs
 from selenium.webdriver.common.by import By
 from time import sleep
+import time
 
 def IgnoreException(proc):
     flg = True
@@ -37,9 +38,9 @@ def Get(patentNum: str):
         .find_element(By.ID, "p00_searchBtn_btnDocInquiry") \
         .click()
 
-    sleep(0.5) # 読み込みに伴い、0.5秒スリープする
-
     for i, num in enumerate(str.split(patentNum, " ")):
+
+        if not num: continue
 
         # 経過情報ボタンを押下する
         IgnoreException(lambda: \
@@ -59,7 +60,14 @@ def Get(patentNum: str):
                 .click()
         )
 
-        sleep(0.8)
+        sleep(0.5)
+
+        # ファイルサイズが50kBを超えるか10秒経過するかしてから次のステップに進む
+        t = time.time()
+        while len(driver.page_source) < 50 * 10e2:
+            if time.time() - t > 10:
+                print(f"{num} : 10秒経過しましたが、htmlファイルのバイト数が50kBを越えませんでした。")
+                break
 
         # htmlファイルを保存する
         with open(f"./html/page-{num}.html", "w", encoding="utf-8") as f:
